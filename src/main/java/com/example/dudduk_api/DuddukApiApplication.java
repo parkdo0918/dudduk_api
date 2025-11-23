e6a6ad08a2bead20c0bfff6509d695d84caee8b4
박도현 <parkdo0918@gmail.com>
2025-11-23 21:38:44 +0900

    chore: Initial

---
+++ src/main/java/com/example/dudduk_api/DuddukApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.dudduk_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuddukApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuddukApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dudduk_api/DuddukApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dudduk_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuddukApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
